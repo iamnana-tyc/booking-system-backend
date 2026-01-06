cbb154f28a2030249ea82a218d9b709a463c2820
iamnana-tyc <nanatyc7@gmail.com>
2026-01-06 22:10:24 +0300

    Add API exception class

---
+++ src/main/java/com/iamnana/booking/exception/APIException.java
@@ -0,0 +1,12 @@
+package com.iamnana.booking.exception;
+
+public class APIException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public APIException() {
+    }
+
+    public APIException(String message) {
+        super(message);
+    }
+}
