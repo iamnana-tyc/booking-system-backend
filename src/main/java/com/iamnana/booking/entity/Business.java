9a2367e2e2626325518f17f9f048a1d4cf629145
iamnana-tyc <nanatyc7@gmail.com>
2026-01-06 22:06:09 +0300

    Add Booking class

---
+++ src/main/java/com/iamnana/booking/entity/Business.java
@@ -0,0 +1,28 @@
+package com.iamnana.booking.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "businesses")
+public class Business extends BaseEntity {
+
+    @NotBlank(message = "Business name is required")
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+}
