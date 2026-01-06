591114f9515c16e3ac63239504897169da0c32fe
iamnana-tyc <nanatyc7@gmail.com>
2026-01-06 22:11:40 +0300

    Repository for service offering

---
+++ src/main/java/com/iamnana/booking/repository/ServiceOfferingRepository.java
@@ -0,0 +1,7 @@
+package com.iamnana.booking.repository;
+
+import com.iamnana.booking.entity.ServiceOffering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceOfferingRepository extends JpaRepository<ServiceOffering, Long> {
+}
