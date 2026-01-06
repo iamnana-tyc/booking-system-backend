d664962879923fafa548278450efc23d3699ab13
iamnana-tyc <nanatyc7@gmail.com>
2026-01-06 22:08:39 +0300

    Add response for api exception

---
+++ src/main/java/com/iamnana/booking/dto/APIResponse.java
@@ -0,0 +1,13 @@
+package com.iamnana.booking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    private String message;
+    private boolean status;
+}
