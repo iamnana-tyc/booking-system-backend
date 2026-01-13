52a19d6045eb4c435e1f51bf3e015816b0b763dc
iamnana-tyc <nanatyc7@gmail.com>
2026-01-13 15:28:27 +0300

    Implement business controller

--- src/main/java/com/iamnana/booking/controller/BusinessController.java
+++ src/main/java/com/iamnana/booking/controller/BusinessController.java
@@ -1,14 +1,13 @@
 package com.iamnana.booking.controller;
 
+import com.iamnana.booking.config.AppConstant;
 import com.iamnana.booking.dto.BusinessRequest;
-import com.iamnana.booking.dto.BusinessResponse;
+import com.iamnana.booking.dto.BusinessResponseDto;
 import com.iamnana.booking.service.BusinessService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,12 +22,28 @@ public class BusinessController {
         return new ResponseEntity<>(business, HttpStatus.CREATED);
     }
 
-    @GetMapping("public/business")
-    public ResponseEntity<List<BusinessResponse>> getAllBusinesses() {
-        List<BusinessResponse> business = businessService.getAllBusinesses();
+    @GetMapping("/business")
+    public ResponseEntity<BusinessResponseDto> getAllBusinesses(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER,  required = false) int pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstant.PAGE_SIZE,  required = false) int pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstant.SORT_BUSINESS_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstant.SORT_DIRECTION, required = false) String sortOrder
+    ) {
+        BusinessResponseDto business = businessService.getAllBusinesses(pageNumber,pageSize,sortBy,sortOrder);
 
         return new ResponseEntity<>(business, HttpStatus.OK);
     }
 
+    @GetMapping("/business/{search}")
+    public ResponseEntity<BusinessResponseDto> searchBusinesses(
+            @PathVariable String search,
+            @RequestParam(name = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER,  required = false) int pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstant.PAGE_SIZE,  required = false) int pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstant.SORT_BUSINESS_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstant.SORT_DIRECTION, required = false) String sortOrder) {
+        BusinessResponseDto business = businessService.searchBusinesses(search, pageNumber, pageSize, sortBy, sortOrder);
+
+        return new ResponseEntity<>(business, HttpStatus.OK);
+    }
 
 }
