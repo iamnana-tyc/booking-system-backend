478520766a27031bd7194ed7464a2a51a7f315cc
iamnana-tyc <nanatyc7@gmail.com>
2026-01-13 14:47:33 +0300

    Add query methods

--- src/main/java/com/iamnana/booking/repository/ServiceOfferingRepository.java
+++ src/main/java/com/iamnana/booking/repository/ServiceOfferingRepository.java
@@ -1,7 +1,16 @@
 package com.iamnana.booking.repository;
 
 import com.iamnana.booking.entity.ServiceOffering;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ServiceOfferingRepository extends JpaRepository<ServiceOffering, Long> {
+    @Query("SELECT s FROM ServiceOffering s WHERE s.name = ?1 AND s.business.id = ?2")
+    ServiceOffering findByNameAndBusinessId(String name, Long businessId);
+
+    Page<ServiceOffering> findByBusinessId(Long businessId, Pageable pageable);
+
+    Page<ServiceOffering> findByNameContainingIgnoreCase(String search, Pageable pageDetails);
 }
