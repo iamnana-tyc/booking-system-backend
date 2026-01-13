d534a2d7bb0b4b32de1a39f91abd50224790f996
iamnana-tyc <nanatyc7@gmail.com>
2026-01-13 14:55:04 +0300

    Constants for pagination

---
+++ src/main/java/com/iamnana/booking/config/AppConstant.java
@@ -0,0 +1,8 @@
+package com.iamnana.booking.config;
+
+public class AppConstant {
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "5";
+    public static final String SORT_SERVICE_OFFERING_BY= "id";
+    public static final String SORT_DIRECTION = "asc";
+}
