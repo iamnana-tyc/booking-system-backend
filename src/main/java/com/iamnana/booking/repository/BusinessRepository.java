caa1b270def2e0de6c8a86fa114662a8970f3de8
iamnana-tyc <nanatyc7@gmail.com>
2026-01-13 15:35:14 +0300

    Add business repository

---
+++ src/main/java/com/iamnana/booking/repository/BusinessRepository.java
@@ -0,0 +1,12 @@
+package com.iamnana.booking.repository;
+
+import com.iamnana.booking.entity.Business;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusinessRepository extends JpaRepository<Business, Long> {
+    Business findBusinessByName(String name);
+
+    Page<Business> findByNameContainingIgnoreCase(String search, Pageable pageable);
+}
