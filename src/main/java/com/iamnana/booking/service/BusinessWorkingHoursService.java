628b52c406ca5d6f037e8c80e939b2c707d684b3
iamnana-tyc <nanatyc7@gmail.com>
2026-01-23 17:17:08 +0300

    Add interface methods

--- src/main/java/com/iamnana/booking/service/BusinessWorkingHoursService.java
+++ src/main/java/com/iamnana/booking/service/BusinessWorkingHoursService.java
@@ -1,7 +1,9 @@
 package com.iamnana.booking.service;
 
+import com.iamnana.booking.dto.BusinessWorkingHoursPatchRequest;
 import com.iamnana.booking.dto.BusinessWorkingHoursRequest;
 import com.iamnana.booking.dto.BusinessWorkingHoursResponse;
+
 
 import java.util.List;
 
@@ -9,4 +11,8 @@ public interface BusinessWorkingHoursService {
     BusinessWorkingHoursResponse createBusinessWorkingHours(Long businessId, BusinessWorkingHoursRequest request);
 
     List<BusinessWorkingHoursResponse> getBusinessWorkingHours(Long businessId);
+
+    BusinessWorkingHoursResponse partialUpdateBusinessWorkingHours(Long businessId, BusinessWorkingHoursPatchRequest request, Long workingHoursId);
+
+    void deleteBusinessWorkingHours(Long businessId, Long workingHoursId);
 }
