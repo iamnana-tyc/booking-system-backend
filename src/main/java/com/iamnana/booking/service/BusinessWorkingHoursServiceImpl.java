90b51653e52dd99896f7b1c69b6d27996ec46edd
iamnana-tyc <nanatyc7@gmail.com>
2026-01-23 17:16:47 +0300

    Handle validations well

--- src/main/java/com/iamnana/booking/service/BusinessWorkingHoursServiceImpl.java
+++ src/main/java/com/iamnana/booking/service/BusinessWorkingHoursServiceImpl.java
@@ -1,5 +1,6 @@
 package com.iamnana.booking.service;
 
+import com.iamnana.booking.dto.BusinessWorkingHoursPatchRequest;
 import com.iamnana.booking.dto.BusinessWorkingHoursRequest;
 import com.iamnana.booking.dto.BusinessWorkingHoursResponse;
 import com.iamnana.booking.entity.Business;
@@ -8,10 +9,12 @@ import com.iamnana.booking.exception.APIException;
 import com.iamnana.booking.exception.ResourceNotFoundException;
 import com.iamnana.booking.repository.BusinessRepository;
 import com.iamnana.booking.repository.BusinessWorkingHoursRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
@@ -27,7 +30,7 @@ public class BusinessWorkingHoursServiceImpl implements BusinessWorkingHoursServ
                 .orElseThrow(() -> new ResourceNotFoundException("Business", "businessId", businessId));
 
         // Prevent duplicate working hours for the same business and day
-        Boolean existWorkingHours = businessWorkingHoursRepository.existsByBusinessAndDayOfWeek(business, request.getDayOfWeek());
+        boolean existWorkingHours = businessWorkingHoursRepository.existsByBusinessAndDayOfWeek(business, request.getDayOfWeek());
         if (existWorkingHours) {
             throw new APIException("Business working hours already exists for " + request.getDayOfWeek() + " for this business" );
         }
@@ -51,10 +54,63 @@ public class BusinessWorkingHoursServiceImpl implements BusinessWorkingHoursServ
         businessRepository.findById(businessId)
                 .orElseThrow(() -> new ResourceNotFoundException("Business", "businessId", businessId));
 
-        List<BusinessWorkingHours> workingHours = businessWorkingHoursRepository.findAll();
+        List<BusinessWorkingHours> workingHours = businessWorkingHoursRepository.findByBusinessId(businessId);
 
         return workingHours.stream()
                 .map(workingHour -> modelMapper.map(workingHour, BusinessWorkingHoursResponse.class))
                 .toList();
+    }
+
+    @Transactional
+    @Override
+    public BusinessWorkingHoursResponse partialUpdateBusinessWorkingHours(Long businessId, BusinessWorkingHoursPatchRequest request, Long workingHoursId) {
+        Business business = businessRepository.findById(businessId)
+                .orElseThrow(() -> new ResourceNotFoundException("Business", "businessId", businessId));
+
+        BusinessWorkingHours existingWorkHours = businessWorkingHoursRepository
+                .findByIdAndBusinessId(workingHoursId, businessId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "BusinessWorkingHours", "workingHoursId", workingHoursId));
+
+        if (request.getDayOfWeek() != null &&
+                !request.getDayOfWeek().equals(existingWorkHours.getDayOfWeek()) &&
+                businessWorkingHoursRepository.existsByBusinessAndDayOfWeek(business, request.getDayOfWeek())) {
+            throw new APIException("Working hours already exist for this day");
+        }
+
+        LocalTime openTime = request.getOpenTime() != null
+                ? request.getOpenTime()
+                : existingWorkHours.getOpenTime();
+
+        LocalTime closeTime = request.getCloseTime() != null
+                ? request.getCloseTime()
+                : existingWorkHours.getCloseTime();
+
+        if (!openTime.isBefore(closeTime)) {
+            throw new APIException("Open time must be before close time");
+        }
+
+        if (request.getDayOfWeek() != null)
+            existingWorkHours.setDayOfWeek(request.getDayOfWeek());
+        if (request.getOpenTime() != null)
+            existingWorkHours.setOpenTime(request.getOpenTime());
+        if (request.getCloseTime() != null)
+            existingWorkHours.setCloseTime(request.getCloseTime());
+
+        BusinessWorkingHours saveUpdatedBusinessWorkingHours =
+                businessWorkingHoursRepository.save(existingWorkHours);
+
+        return modelMapper.map(saveUpdatedBusinessWorkingHours, BusinessWorkingHoursResponse.class);
+    }
+
+    @Transactional
+    @Override
+    public void deleteBusinessWorkingHours(Long businessId, Long workingHoursId) {
+        BusinessWorkingHours workingHours =
+                businessWorkingHoursRepository.findByIdAndBusinessId(workingHoursId, businessId)
+                        .orElseThrow(() -> new ResourceNotFoundException(
+                                "BusinessWorkingHours", "workingHoursId", workingHoursId));
+
+        businessWorkingHoursRepository.delete(workingHours);
     }
 }
