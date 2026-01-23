49af1594ce1ba11f3e61dc205a3c9f776114735a
iamnana-tyc <nanatyc7@gmail.com>
2026-01-23 17:15:29 +0300

    Add delete and update to controller method

--- src/main/java/com/iamnana/booking/controller/BusinessWorkingHoursController.java
+++ src/main/java/com/iamnana/booking/controller/BusinessWorkingHoursController.java
@@ -1,5 +1,6 @@
 package com.iamnana.booking.controller;
 
+import com.iamnana.booking.dto.BusinessWorkingHoursPatchRequest;
 import com.iamnana.booking.dto.BusinessWorkingHoursRequest;
 import com.iamnana.booking.dto.BusinessWorkingHoursResponse;
 import com.iamnana.booking.service.BusinessWorkingHoursService;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class BusinessWorkingHoursController {
 
@@ -39,5 +40,25 @@ public class BusinessWorkingHoursController {
         return ResponseEntity.ok(businessResponse);
     }
 
+    @PatchMapping("/businesses/{businessId}/working-hours/{workingHoursId}")
+    public ResponseEntity<BusinessWorkingHoursResponse> partialUpdateBusinessWorkingHours(
+            @PathVariable Long businessId,
+            @Valid @RequestBody BusinessWorkingHoursPatchRequest request,
+            @PathVariable Long workingHoursId
+    ){
+        BusinessWorkingHoursResponse businessResponse = businessWorkingHoursService
+                .partialUpdateBusinessWorkingHours(businessId, request, workingHoursId);
 
+        return ResponseEntity.ok(businessResponse);
+    }
+
+    @DeleteMapping("/businesses/{businessId}/working-hours/{workingHoursId}")
+    public ResponseEntity<String> deleteBusinessWorkingHours(
+            @PathVariable Long businessId,
+            @PathVariable Long workingHoursId
+    ){
+        businessWorkingHoursService.deleteBusinessWorkingHours(businessId,workingHoursId);
+
+        return ResponseEntity.ok("Successfully deleted business working hours");
+    }
 }
