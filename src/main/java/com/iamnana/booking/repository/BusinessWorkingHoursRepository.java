94b034e8fcefbb90839f2435fdbb61d1b0b38427
iamnana-tyc <nanatyc7@gmail.com>
2026-01-23 17:16:02 +0300

    Add methods to repo

--- src/main/java/com/iamnana/booking/repository/BusinessWorkingHoursRepository.java
+++ src/main/java/com/iamnana/booking/repository/BusinessWorkingHoursRepository.java
@@ -6,7 +6,14 @@ import com.iamnana.booking.entity.BusinessWorkingHours;
 import com.iamnana.booking.entity.DayOfWeek;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BusinessWorkingHoursRepository extends JpaRepository<BusinessWorkingHours, Long> {
 
     Boolean existsByBusinessAndDayOfWeek(Business business, DayOfWeek dayOfWeek);
+
+    List<BusinessWorkingHours> findByBusinessId(Long businessId);
+
+    Optional<BusinessWorkingHours> findByIdAndBusinessId(Long workingHoursId, Long businessId);
 }
